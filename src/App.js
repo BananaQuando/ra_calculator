9d189c0a5a5a15f5ab0f0a3d6bfd88520154a708
PEDROMACHO <sindrome15@mail.ru>
2020-04-12 13:56:17 +0700

    add blocks

--- src/App.js
+++ src/App.js
@@ -9,17 +9,36 @@ function App() {
           <div className="card-hover-shadow-2x mb-3 mt-3 card">
             <div className="card-header">Calculator</div>
             <div className="card-body">
-              asdasd
+              <div className="drag-and-drop">
+                  <div className="drag-and-drop__text">
+                    Drop tasks
+                  </div>
+              </div>
             </div>
             <div className="d-block text-right card-footer">
-              <button className="mr-2 btn btn-link btn-sm">Cancel</button>
-              <button className="btn-shadow-primary btn btn-primary btn-lg">Submit</button>
+              <div className="widget-content p-0">
+                <div className="widget-content-outer">
+                  <div className="widget-content-wrapper">
+                    <div className="widget-content-left">
+                      <div className="widget-heading">Total Orders</div>
+                    </div>
+                    <div className="widget-content-right">
+                      <div className="widget-numbers text-success">
+                        <small>$</small>
+                        0
+                    </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
         <div className="col-md-4">
           <div className="card-hover-shadow-2x mb-3 mt-3 card">
-            тут карточки задач по группам
+            <div className="card-body">
+              тут карточки задач по группам
+            </div>
         </div>
         </div>
       </div>
