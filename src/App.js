cfc82a4cabdd08b0fe1021c5fca1e2959860f1ec
PEDROMACHO <sindrome15@mail.ru>
2020-04-12 13:29:08 +0700

    init and another

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card-hover-shadow-2x mb-3 mt-3 card">
+            <div className="card-header">Calculator</div>
+            <div className="card-body">
+              asdasd
+            </div>
+            <div className="d-block text-right card-footer">
+              <button className="mr-2 btn btn-link btn-sm">Cancel</button>
+              <button className="btn-shadow-primary btn btn-primary btn-lg">Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
